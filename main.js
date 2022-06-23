6f982691ae1786812f08866ac5155a7264959d04
yungit4455 <yjhama1532@gmail.com>
2022-06-24 00:41:40 +0900

    생활코딩 nodejs 18까지. 파일 읽기, 웹 페이지 종료하지 않고 본문 내용 변경, 콘솔 매개변수

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function(request,response){
-  const _url = request.url;
-  const queryData = url.parse(_url, true).query;
-  let title = queryData.id;
-  
-  const template = `
-  
-  `;
-  response.end(template);  
-});
+const app = http.createServer(function(request,response) {
+    let _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    let title = queryData.id;
+
+    if( _url == '/?id=index' ) {
+        title = 'Welcome';
+    }
+    if( _url == 'favicon.ico' ) {
+        return response.writeHead( 404 );
+    }
+    response.writeHead( 200 );
+
+    fs.readFile( `data/${queryData.id}`, 'utf-8', ( err, description ) => {
+    // if( err ) throw err;
+    let template = `
+        <!doctype html>
+        <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/?id=index">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+            </body>
+        </html>
+    `;
+    response.end(template);  
+    } );
+} );
 app.listen(3000);
