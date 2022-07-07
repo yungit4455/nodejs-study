e2e5c20fca11efb227be6f8420da3ba30c2d3bdb
yungit4455 <yjhama1532@gmail.com>
2022-07-07 15:44:22 +0900

    정적 파일 서비스

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const topic = require('./lib/topic');
-const author = require('./lib/author');
-const bodyParser = require('body-parser');
-
+const topic = require('./lib/topic')
+const author = require('./lib/author')
+const bodyParser = require('body-parser')
 const compression = require('compression')
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 // 데이터를 압축하여 네트워크 트래픽을 줄이는 서드파티 라이브러리
 app.use(compression())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     topic.home(req, res)
